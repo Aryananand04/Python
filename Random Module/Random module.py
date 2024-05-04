7d62f384c56feeb4aecbb88f5911b295ae3d2be9
Aryananand04 <aryananu42@gmail.com>
2024-05-04 21:05:50 +0530

    random float

--- Random Module/Random module.py
+++ Random Module/Random module.py
@@ -1,5 +1,8 @@
 import random
-import my_module
+# import my_module
 random_integer = random.randint(1,10)
 print(random_integer)
-print(my_module.pi)
+# print(my_module.pi)
+
+random_float = random.random()
+print(random_float)
